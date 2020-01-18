65cdb91d42ae38334ec815e83bbc8ca4546211d4
Adam Drake <addrake@ebay.com>
2020-01-18 13:55:52 +0100

    Adding redis and cookies

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,53 @@ import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
 
 import { RegisterResolver } from "./modules/user/Register";
+import { redis } from "./redis";
+import { LoginResolver } from "./modules/user/Login";
 
 const main = async () => {
   await createConnection();
+
   const schema = await buildSchema({
-    resolvers: [RegisterResolver]
+    resolvers: [RegisterResolver, LoginResolver]
   });
 
-  const apolloServer = new ApolloServer({ schema });
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req }: any) => ({ req })
+  });
 
   const app = Express();
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000"
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis
+      }),
+      name: "qid",
+      secret: "askl1234",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // 7 years
+      }
+    })
+  );
 
   apolloServer.applyMiddleware({ app });
 
