c9b397147abe002f96b14a24059c75928c96b0c1
Adam Drake <addrake@ebay.com>
2020-03-21 11:59:58 +0100

    adding queries for articles

--- src/modules/article/Article.ts
+++ src/modules/article/Article.ts
@@ -1,10 +1,27 @@
-import { Resolver, Mutation, Arg } from "type-graphql";
+import { Resolver, Mutation, Arg, Query } from "type-graphql";
 
 import { Article } from "../../entity/Article";
 import { ArticleInput } from "./article/ArticleInput";
 
 @Resolver()
 export class ArticleResolver {
+  @Query(() => Article)
+  async getonearticle(@Arg("id") id: string) {
+    const article = await Article.findOne(id);
+    if (article === undefined) {
+      console.log(`The article with id: ${id} can't be found`);
+    }
+
+    return article;
+  }
+
+  @Query(() => [Article])
+  async articles(): Promise<Article[]> {
+    const articles = await Article.find();
+
+    return articles;
+  }
+
   @Mutation(() => Article)
   async article(
     @Arg("data") { title, articleText, author }: ArticleInput
