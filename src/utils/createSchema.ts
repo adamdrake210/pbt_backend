aec01ea31d0ea10a5fcf75195387364e3bf79ad0
Adam Drake <addrake@ebay.com>
2020-03-21 10:45:03 +0100

    testing

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -1,8 +1,12 @@
 import { buildSchema } from "type-graphql";
+import { RegisterResolver } from "../modules/user/Register";
+import { LoginResolver } from "../modules/user/Login";
+import { LogoutResolver } from "../modules/user/Logout";
+import { MeResolver } from "../modules/user/Me";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [__dirname + "/../modules/*/*.ts"],
+    resolvers: [RegisterResolver, LoginResolver, LogoutResolver, MeResolver],
     authChecker: ({ context: { req } }) => {
       if (req.session.userId) {
         return true;
